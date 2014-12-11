b330d80f829e329361a3d748328a15c1e7b838d9
Maxime Kjaer <maxime.kjaer@gmail.com>
2014-12-11 16:19:18 +0100

    Removed "Recent Blog posts" in /about/
    + Changed title
    + Added <abbr> tag in second headline

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -6,19 +6,22 @@ jQuery(document).ready(function($){
     
 //smooth scrolling when clicking on the secondary navigation items
 	secondaryNav.find('ul a').on('click', function(event){
-        event.preventDefault();
-        var target= $(this.hash);
-        var hash = this.hash;
-        $('body,html').animate({
-        	'scrollTop': target.offset().top - secondaryNav.height() + 1
-        	}, 400, function () {
-                location.hash = hash;
-            }
-        ); 
-        //on mobile - close secondary navigation
-        $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
-        secondaryNav.find('ul').removeClass('is-visible');
-        
+        if (!window.location.origin)
+            window.location.origin = window.location.protocol+"//"+window.location.host;
+        if (window.location.origin + '/' != this.href) {
+            event.preventDefault();
+            var target= $(this.hash);
+            var hash = this.hash;
+            $('body,html').animate({
+                'scrollTop': target.offset().top - secondaryNav.height() + 1
+                }, 400, function () {
+                    location.hash = hash;
+                }
+            );
+            //on mobile - close secondary navigation
+            $('.cd-secondary-nav-trigger').removeClass('menu-is-open');
+            secondaryNav.find('ul').removeClass('is-visible');
+        }
     });
     
     if (location.hash != "") {
