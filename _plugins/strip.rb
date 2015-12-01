0d7da55f953600a8bdbb5fa3e6db44187b927096
Maxime Kjaer <maxime.kjaer@gmail.com>
2015-12-01 21:16:58 +0100

    Add strip.rb plugin
    This strips newlines from the generated code.

---
+++ _plugins/strip.rb
@@ -0,0 +1,15 @@
+# Replaces multiple newlines and whitespace 
+# between them with one newline
+# From https://github.com/aucor/jekyll-plugins
+
+module Jekyll
+  class StripTag < Liquid::Block
+
+    def render(context)
+      super.gsub /\n\s*\n/, "\n"
+    end
+
+  end
+end
+
+Liquid::Template.register_tag('strip', Jekyll::StripTag)
