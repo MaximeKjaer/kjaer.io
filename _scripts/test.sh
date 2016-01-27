e251f48056e126b9cc14c26e09847a3b5a5a0f1e
Maxime Kjaer <maxime.kjaer@gmail.com>
2016-01-28 00:28:11 +0100

    Remove erroneous comment and --verbose from the test

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-# Test that all links in our site return a 400, including the favicon
-timeout 30s bundle exec htmlproof _site --only-4xx --external_only --check-html --check-favicon --verbose
+
+timeout 30s bundle exec htmlproof _site --only-4xx --external_only --check-html --check-favicon
