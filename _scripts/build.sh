74ef4b0412572d7ae1c371f90dfc494d958e32fc
Maxime Kjaer <maxime.kjaer@gmail.com>
2016-01-27 22:42:34 +0100

    Fix zopfli command

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,4 +4,4 @@ set -x
 bundle exec jekyll build
 
 # Compress assets with Zopfli
-zopfli/zopfli --i1000 --verbose _site**/*.html  _site**/*.css _site**/*.js  _site**/*.xml
+zopfli/zopfli --i1000 _site/**/*.html _site/*.html  _site/**/*.css _site/*.css _site/**/*.js _site/*.js _site/**/*.xml _site/*.xml
