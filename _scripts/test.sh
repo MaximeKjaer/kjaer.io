439839b463a3a9120e7a9462c59d2dcca7fc6555
Maxime Kjaer <maxime.kjaer@gmail.com>
2016-01-27 22:39:40 +0100

    Try to fix zopfli command

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -x
 # Test that all links in our site return a 400, including the favicon
-# This is a bit complicated, since it raises an error when we add a page. Maybe test internal links locally?
 timeout 20s bundle exec htmlproof _site --only-4xx --check-html --check-favicon --checks-to-ignore "LinkCheck"
 
 # Test that all links in our sitemap.xml and feed.xml return a 400.
