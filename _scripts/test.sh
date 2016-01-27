8385fe19ae9f8666bf89dad5ffe77e06af736e2a
Maxime Kjaer <maxime.kjaer@gmail.com>
2016-01-28 00:16:00 +0100

    Simplify tests

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -x
 # Test that all links in our site return a 400, including the favicon
-timeout 20s bundle exec htmlproof _site --only-4xx --check-html --check-favicon --checks-to-ignore "LinkCheck"
-
-# Test that all links in our sitemap.xml and feed.xml return a 400.
-timeout 20s bundle exec htmlproof _site --ext .xml --only-4xx --checks-to-ignore "LinkCheck"
+timeout 30s bundle exec htmlproof _site --only-4xx --external_only --check-html --check-favicon --verbose
