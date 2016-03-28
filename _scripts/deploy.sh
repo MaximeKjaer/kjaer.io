58551a2ffbd2c173a37510036935f19c9ecc32a8
Maxime Kjaer <maxime.kjaer@gmail.com>
2016-03-28 11:51:00 +0200

    Commit the compression so that it's deployed too

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-	cd _site
 	echo "Deploying to remote"
+	cd _site
+	git add .
+	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
 	git push deploy master
 else
 	echo "Not deploying, since this branch isn't master."
