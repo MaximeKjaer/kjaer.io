e48f33d6c07d19ff83d8fe74b359830c52ef6635
Maxime Kjaer <maxime.kjaer@gmail.com>
2016-07-18 13:18:46 +0200

    Raise timeout limit for Travis tests to 60 seconds

--- _scripts/test.sh
+++ _scripts/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Proofing the HTML"
-timeout 30s bundle exec htmlproofer ./_site --check-html --check-favicon --allow-hash-href --external_only --only-4xx --http-status-ignore 429
+timeout 60s bundle exec htmlproofer ./_site --check-html --check-favicon --allow-hash-href --external_only --only-4xx --http-status-ignore 429
