8cff378881741f7d4153b898aeba88bcb3adee3e
Maxime Kjaer <maxime.kjaer@gmail.com>
2018-02-07 18:57:24 +0100

    Fix build script (#3)
    * Fix current directory when building
    
    * Fix git merge command
    
    * Add package-lock.json to excluded files
    
    * Delete files removed in build

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -10,47 +10,42 @@ modfiles=""
 modimg=""
 modzopfli=""
 
-# Functions
-function init {
-	echo "Initializing git"
-	git init
-	git remote add deploy "deploy@kjaermaxi.me:/var/www/kjaermaxi.me"
-	git config user.name "Travis CI"
-	git config user.email "maxime.kjaer+travisCI@gmail.com"
-	echo "Fetching from remote"
-	git fetch deploy
-	git checkout -b build
-}
-
-function build {
-	echo "Building..."
-	bundle exec jekyll build # Build the site with Jekyll
-	grunt build # Build with Grunt; see Gruntfile.js for more details.
-	echo "Committing the build"
-	git add .
-	git commit -q -m "Build #$TRAVIS_BUILD_NUMBER"
-}
-
-function compare {
-	echo "Comparing this build to the previous one"
-	git checkout master
-	
-	modfiles=$(git diff --name-only master..build | grep -v $gzip_ext)
-	modimg=$(grep $img_ext <<< "$modfiles" | tr '\n' ' ') # Not used right now, but this is a TODO.
-	modzopfli=$(grep $zopfli_ext <<< "$modfiles" | tr '\n' ' ')
-	modfiles=$(echo $modfiles | tr '\n' ' ')
-
-	git merge -X theirs --commit -m "Merge build #$TRAVIS_BUILD_NUMBER" build --allow-unrelated-histories
-}
-
-mkdir _site 
+echo "Initializing git"
+mkdir _site
 cd _site
-init
-build
-compare
+git init
+git remote add deploy "deploy@kjaermaxi.me:/var/www/kjaermaxi.me"
+git config user.name "Travis CI"
+git config user.email "maxime.kjaer+travisCI@gmail.com"
 
-# Compress assets with Zopfli (should always be the last command)
-echo "Compressing the following assets using Zopfli: $modfiles"
+echo "Fetching from remote"
+git fetch deploy
+
+echo "Building"
+git checkout -b build
+cd ..
+bundle exec jekyll build # Build the site with Jekyll
+grunt build # Build with Grunt; see Gruntfile.js for more details.
+
+echo "Committing the build"
+cd _site
+git add .
+git commit -q -m "Build #$TRAVIS_BUILD_NUMBER"
+
+echo "Comparing this build to the previous one"
+git checkout master
+modfiles=$(git diff --name-only master..build | grep -v $gzip_ext)
+modimg=$(grep $img_ext <<< "$modfiles" | tr '\n' ' ') # Not used right now, but this is a TODO.
+modzopfli=$(grep $zopfli_ext <<< "$modfiles" | tr '\n' ' ')
+modfiles=$(echo $modfiles | tr '\n' ' ')
+
+echo "Merging build into master"
+git merge --allow-unrelated-histories -X theirs --commit -m "Merge build #$TRAVIS_BUILD_NUMBER" build
+# 'git merge -X theirs' does not remove deleted files in build, so we must do it manually:
+deleted=$(git diff --name-only master..build | grep -v $gzip_ext | tr '\n' ' ') # deleted1.html deleted2.html
+deleted_gz=$(sed -e 's/ \|$/.gz /g' <<< $deleted) # deleted1.html.gz deleted2.html.gz
+git rm --ignore-unmatch $deleted $deleted_gz
+
+echo "Compressing the following assets using Zopfli: $modzopfli"
 ../zopfli/zopfli --i1000 $modzopfli
-
 cd ..
