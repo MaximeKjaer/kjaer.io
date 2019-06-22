d92d2d3bbec58b4f6adb3ff1a035affbc3a9ab93
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-06-22 23:04:46 +0200

    Refactor deployment condition

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-if [ $TRAVIS_BRANCH == 'master' ] ; then
-	echo "Deploying to remote"
-	cd _site
-	git add .
-	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-	git push deploy master
-else
-	echo "Not deploying, since this branch isn't master."
-fi
+echo "Deploying to remote"
+cd _site
+git add .
+git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
+git push deploy master
