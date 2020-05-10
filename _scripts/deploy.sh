e945ecd1bff3dbba652191fb54ed0c51558229eb
Maxime Kjaer <maxime.kjaer@gmail.com>
2020-05-10 15:34:32 +0200

    Switch deployments to Azure instead of DigitalOcean

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e 
 
-echo "Committing build to master"
-cd _site
-git add .
-git commit -m "Build #$TRAVIS_BUILD_NUMBER"
-
-echo "Deploying to remote"
-git push -u origin master
-cd ..
+echo "Uploading build to Azure"
+az storage blob upload-batch -d \$web -s ./_site
--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e 
 
-echo "Importing the SSH deployment key"
-openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in raindrop-deploy.enc -out raindrop-deploy -d
-rm raindrop-deploy.enc
-chmod 600 raindrop-deploy
-mv raindrop-deploy ~/.ssh/id_rsa
-
-echo "Setting up git"
-git config user.name "Travis CI"
-git config user.email "maxime.kjaer+travisCI@gmail.com"
-git clone --depth 1 --single-branch deploy@kjaer.io:/var/www/kjaer.io/ _site
+echo "Installing Azure CLI"
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install apt-transport-https azure-cli
+az login -u $AZ_LOGIN_NAME -p $AZ_PASSWORD --service-principal --tenant $AZ_TENANT > /dev/null 2>&1
 
 echo "Installing dependencies"
 gem install bundler # Install bundler 2.0, which will be used by Travis in `install` phase
