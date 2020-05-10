4c27354d30cd995bd9233e638de9c345b5199475
Maxime Kjaer <maxime.kjaer@gmail.com>
2020-05-10 16:19:04 +0200

    Fix deployment script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,4 +2,4 @@
 set -e 
 
 echo "Uploading build to Azure"
-az storage blob upload-batch -d \$web -s ./_site
+az storage blob upload-batch --account-name $AZURE_STORAGE_ACCOUNT --source ./_site --destination \$web
