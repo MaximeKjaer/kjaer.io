f906b4fe53d828026ca4dc32976d35695cc5dc89
Maxime Kjaer <maxime.kjaer@gmail.com>
2020-05-10 16:31:40 +0200

    Disable JSON response printing of deployment script

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -2,4 +2,4 @@
 set -e 
 
 echo "Uploading build to Azure"
-az storage blob upload-batch --account-name $AZURE_STORAGE_ACCOUNT --source ./_site --destination \$web
+az storage blob upload-batch --account-name $AZURE_STORAGE_ACCOUNT --source ./_site --destination \$web --output none
